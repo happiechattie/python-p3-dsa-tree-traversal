a0508e43f90d7fef2411c1dae36a541d7155a527
Christine Baik <christuaik@gmail.com>
2023-09-14 16:22:05 -0700

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,14 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, search):
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+        node = nodes_to_visit.pop(0)
+        if search == node["id"]:
+          return node
+        
+        nodes_to_visit = node['children'] + nodes_to_visit
+
+    return None
